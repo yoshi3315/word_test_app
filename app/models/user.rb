d227ddd877fb3ca9d92ae95921f14780f4ad489a
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-25 09:02:39 +0000

    userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true,
+                   length: { maximum: 50 },
+                   uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
