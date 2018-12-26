13e93341ddec48d08705159b707e16b12efe0dcd
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-26 03:14:13 +0000

    フラッシュメッセージの記載方法を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'ユーザー新規登録が完了しました！ようこそ単語帳へ！'
-      redirect_to root_url
+      redirect_to root_url, flash: {
+        success: 'ユーザー新規登録が完了しました！ようこそ単語帳へ！'
+      }
     else
       render 'new'
     end
