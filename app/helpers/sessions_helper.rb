4d6290b727020e9e2b0e9334f8e2d59f6c2fcaef
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-26 08:45:57 +0000

    sessionsコントローラーで使用するログイン、永続クッキーまわりのヘルパーメソッドを定義

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
 end
