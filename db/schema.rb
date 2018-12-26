177ac957024a39955f8a41e1a3d50422ff426f88
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-26 09:01:07 +0000

    暗号化トークン記憶用のカラムをusersテーブルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_26_013001) do
+ActiveRecord::Schema.define(version: 2018_12_26_085903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_12_26_013001) do
     t.string "password_digest", comment: "暗号化パスワード"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
 end
