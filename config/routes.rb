abe3bc4dfbd63331123dd10e98b268efaf02062e
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-26 05:40:34 +0000

    新規登録エラー時のリロードでルーティングエラーになるのを回避

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:new, :create]
-  get '/users', to: 'users#new'
 
+  match '*path' => 'application#error404', via: :all
 end
