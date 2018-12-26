24ad17ada3fefdb8f4deea7c43d179c6a9c4d8ba
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-26 04:12:06 +0000

    userモデルのバリデーション記載方法を編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,6 @@ class User < ApplicationRecord
   validates :name, presence: true,
                    length: { maximum: 50 },
                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
