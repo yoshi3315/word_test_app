30aeb82f29ffe4c330b4d6ef1679fb0db9b2ef43
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-26 08:40:40 +0000

    sessions#new, create, destroyアクションを定義し、createアクションの詳細を記載

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:session][:name].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to root_url, flash: { success: t('views.flash.login_success') }
+    else
+      flash.now[:danger] = t('views.flash.login_danger')
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
