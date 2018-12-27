c94b93a0863b375cf3d29e7ea30315785a312406
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-27 04:06:13 +0000

    ユーザー新規登録時にログインするよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       redirect_to root_url, flash: { success: t('views.flash.create_success') }
     else
       render 'new'
