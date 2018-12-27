3ec40c04029232c8c1a58daf4d25de40378e54b3
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-27 11:16:36 +0000

    sessionコントローラー、ヘルパー、ビューを戻す

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :redirect_logged_in_user_to_root_page, only: %i[new create]
-
   def index
     @users = User.all
   end
