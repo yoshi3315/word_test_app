a38f0f2bdbbf7a39a1536ae94648bcaf06a26ef6
yoshi3315 <yoshiki.chiba@one-star.jp>
2019-01-07 06:38:51 +0000

    rubocopエラーを自動修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Question < ApplicationRecord
   with_options presence: true do
     validates :question
--- db/migrate/20190107042704_create_questions.rb
+++ db/migrate/20190107042704_create_questions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions, comment: '単語マスタ' do |t|
