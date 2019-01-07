736b0dc1fdf58e8d48fb2c0993bed8f49c0fea6f
yoshi3315 <yoshiki.chiba@one-star.jp>
2019-01-07 06:40:06 +0000

    gem_annotateで追記

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: questions # 単語マスタ
+#
+#  id          :bigint(8)        not null, primary key
+#  question    :string                                 # 単語
+#  description :text                                   # 和訳
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+
 class Question < ApplicationRecord
   with_options presence: true do
     validates :question
