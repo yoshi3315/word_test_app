8ab0e1ccc146b7c031bbc0c6672e585ff4b609af
yoshi3315 <yoshiki.chiba@one-star.jp>
2019-01-09 02:44:46 +0000

    rubocopエラーを手動修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -22,11 +22,11 @@ class Question < ApplicationRecord
     validates :description
   end
 
-  def self.ransackable_attributes(auth_object = nil)
+  def self.ransackable_attributes(_auth_object = nil)
     %w[question description]
   end
 
-  def self.ransackable_associations(auth_object = nil)
+  def self.ransackable_associations(_auth_object = nil)
     []
   end
 end
