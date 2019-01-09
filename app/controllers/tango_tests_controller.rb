3bc20faab20f6e635d07360e957746368dd66b48
yoshi3315 <yoshiki.chiba@one-star.jp>
2019-01-09 03:45:07 +0000

    50コ以上の単語が登録されていなかったらルートページに戻すフィルターを設定

--- app/controllers/tango_tests_controller.rb
+++ app/controllers/tango_tests_controller.rb
@@ -1,7 +1,15 @@
 class TangoTestsController < ApplicationController
   before_action :redirect_non_logged_in_user_to_login_page
+  before_action :questions_number_valid?
 
   def index
     @question = Question.find(1)
+  end
+
+  private
+
+  def questions_number_valid?
+    msg = t('views.flash.number_danger')
+    redirect_to root_url, flash: { danger: msg } unless Question.count >= 50
   end
 end
