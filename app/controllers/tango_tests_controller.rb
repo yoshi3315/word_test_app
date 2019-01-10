ec95266d681d259141bc3c22e4b7101fd4f33744
yoshi3315 <yoshiki.chiba@one-star.jp>
2019-01-10 05:32:03 +0000

    sessionで保存しているkeyの名称を変更

--- app/controllers/tango_tests_controller.rb
+++ app/controllers/tango_tests_controller.rb
@@ -10,7 +10,7 @@ class TangoTestsController < ApplicationController
     if session[:question_no] >= 50
       redirect_to root_url
     else
-      @question = Question.where.not(id: session[:previously_question_id]).sample
+      @question = Question.where.not(id: session[:already_used_questions]).sample
       @dummies = Question.where.not(id: @question.id).sample(2)
       @choices = (@dummies << @question).shuffle!
     end
@@ -25,11 +25,11 @@ class TangoTestsController < ApplicationController
 
   def function_of_remembering_already_used_questions_and_number_of_questions
     if request.xhr?
-      session[:previously_question_id] << params[:question_id]
-      session[:question_no] += 1
+      session[:already_used_questions] << params[:question_id]
+      session[:number_of_questions] += 1
     else
-      session[:previously_question_id] = []
-      session[:question_no] = 0
+      session[:already_used_questions] = []
+      session[:number_of_questions] = 0
     end
   end
 end
