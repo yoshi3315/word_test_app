7328868fae1060cf43b43332f6676597036ca9ad
yoshi3315 <yoshiki.chiba@one-star.jp>
2019-01-11 08:55:39 +0000

    rubocopエラーを自動修正

--- db/migrate/20190111054811_add_column_highest_rate_to_users.rb
+++ db/migrate/20190111054811_add_column_highest_rate_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddColumnHighestRateToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :highest_rate, :integer
--- db/migrate/20190111055017_change_column_highest_rate_to_users.rb
+++ db/migrate/20190111055017_change_column_highest_rate_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeColumnHighestRateToUsers < ActiveRecord::Migration[5.2]
   def change
     change_column :users, :highest_rate, :integer, comment: '単語帳最高正解率'
